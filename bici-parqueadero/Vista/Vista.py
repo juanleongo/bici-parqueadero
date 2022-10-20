708b7e648e9058dc230ed7e21fee970f0c5bea9f
juanleongo <69259203+juanleongo@users.noreply.github.com>
2022-10-20 09:31:18 -0500

    modificacion vista principal

--- bici-parqueadero/Vista/Vista.py
+++ bici-parqueadero/Vista/Vista.py
@@ -1,30 +1,47 @@
-from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+import tkinter as tk
+from tkinter import ttk,messagebox
 
-class cuadricula(GridLayout):
-    def __init__(self, **kwargs):
-        super(cuadricula, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-        self.add_widget(Label(text='password'))
-        self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
+class Inicio(tk.Tk):
+
+    def __init__(self):
+        super().__init__()
+    
+
+ 
+        self.geometry('800x600')
+        self.title('BICI-PARKING')
+        self.configure(bg = 'beige')
+        self.resizable(0,0)
+
+        self.rowconfigure(0,weight=5)
+        self.rowconfigure(1,weight=1)
+        self.rowconfigure(2,weight=1)
+        self.columnconfigure(0,weight=1)
+        self.columnconfigure(1,weight=1)
+
+        self._crear_componentes()
 
 
+    def _crear_componentes(self):
 
+        letrero = tk.Label(self,text="BIENVENIDOS A BICI-PARKING-UD",font=("Helvetica",14))
+        letrero.grid(row=0,column=0,columnspan=3)
 
+        def registrar():
+            messagebox.showinfo('hola')
+            
 
-        #return Button(text="hola", background_color=(155,0,0,80))
-class kivyHola(App):
+        registro = ttk.Button(self,text='REGISTRAR', command=registrar)
+        registro.grid(row=1,column=0 ,sticky='NWES',padx=10)
 
-    def build(self):
+        def ingresar():
+            messagebox.showinfo('hola2')
+            
 
-        return  cuadricula()
+        ingreso = ttk.Button(self,text='INICIAR SESION', command=ingresar)
+        ingreso.grid(row=1,column=1 ,sticky='NWES',padx=10)
+
 
 if __name__ == '__main__':
-    kivyHola().run()
+    principal = Inicio()
+    principal.mainloop()
